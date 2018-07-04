00b7a5fa811e16af100d9f5bb0a5b7d1397da561
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-07-04 08:29:46 -0700

    removed all things related to messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Message.create([{body: "Hello, World!"}, {body: "You did it!"}])
